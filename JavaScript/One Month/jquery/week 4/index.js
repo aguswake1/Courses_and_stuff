23fd458d7e8537af44cfd2897ed0db7437ff90fe
Agustin Olano <agustinolanoit@gmail.com>
2021-02-12 09:53:02 -0300

    se empieza proyecto 4

--- JavaScript/One Month/jquery/week 4/index.js
+++ JavaScript/One Month/jquery/week 4/index.js
@@ -1,5 +1,15 @@
-$(document).ready(function() {
-    // YOUR CODE HERE!
+$(document).ready(function () {
+    $.ajax({
+        url: "https://api.jsonbin.io/b/5e99f36b5fa47104cea282ff/4",
+        dataType: "json",
+        success: function (response) {
+            $.each(response.apartments, function (index, apartment) {
+                var listing = '<a href = "#" class = "list-group-item"><h4 class="list-group-item-heading">' + apartment.description + ' / ' + apartment.bedrooms + ' / ' + apartment.price + '</h4><p class="list-group-item-text">' + apartment.neighborhood + '</p></a>';
+                $(".apartments").append(listing);
+            })
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
 });
-
-

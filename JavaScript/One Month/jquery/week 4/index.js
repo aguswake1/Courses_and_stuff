96ac7e102437820d3e69dc6a36150d7e8b85917d
Agustin Olano <agustinolanoit@gmail.com>
2021-02-13 16:30:41 -0300

    finalizo la semana 4 de jquery

--- JavaScript/One Month/jquery/week 4/index.js
+++ JavaScript/One Month/jquery/week 4/index.js
@@ -4,7 +4,8 @@ $(document).ready(function () {
         dataType: "json",
         success: function (response) {
             $.each(response.apartments, function (index, apartment) {
-                var listing = '<a href = "#" class = "list-group-item"><h4 class="list-group-item-heading">' + apartment.description + ' / ' + apartment.bedrooms + ' / ' + apartment.price + '</h4><p class="list-group-item-text">' + apartment.neighborhood + '</p></a>';
+                var apartmentClass = apartment.city.toLowerCase().replace(" ", "-");
+                var listing = '<a href = "#" id="' + apartment.id + '" class = "list-group-item ' + apartmentClass + ' listings"><h4 class="list-group-item-heading">' + apartment.description + ' / ' + apartment.bedrooms + ' / ' + apartment.price + '</h4><p class="list-group-item-text">' + apartment.neighborhood + '</p></a>';
                 $(".apartments").append(listing);
             })
         },
@@ -12,4 +13,37 @@ $(document).ready(function () {
             console.log(error);
         }
     });
+    $(".filter").click(function () {
+        $(".listings").show();
+
+        $(".filter").removeClass("active");
+        $(this).addClass("active");
+
+        var city = $(this).attr("id");
+
+        if (city !== "all") {
+            $(".listings").not("." + city).css("display", "none");
+        }
+    });
+
+    $(document).on("click", ".listings", function () {
+        var id = $(this).attr("id");
+        $.ajax({
+            url: "https://api.jsonbin.io/b/5e99f36b5fa47104cea282ff/4",
+            dataType: "json",
+            success: function (response) {
+                var selectedApartment = $.grep(response.apartments, function (apartment) {
+                    return apartment.id == id
+                });
+
+                address = selectedApartment[0].address;
+                /* abre la nueva ventana */
+                window.open("http://maps.google.com/?q=" + address);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    });
+
 });
